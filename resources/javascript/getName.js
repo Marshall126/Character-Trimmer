8f08b5660117ae14b3e4250f1e2700921933cd87
Student <student@MacBook-Pro-2457-Painter-IndpndtSTUDY.local>
2018-05-09 14:39:00 -0500

    First commit

---
+++ resources/javascript/getName.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.nameButton').click(()=>{
+        let nameGot = $('.name').value;
+        alert(nameGot);
+    });
+})
+function getName(){
+    
+    return $(nameGot)
+}
