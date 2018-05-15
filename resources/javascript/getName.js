8ebf596b742cffc52c3599ae374efad8eba895d6
Student <student@MacBook-Pro-2457-Painter-IndpndtSTUDY.local>
2018-05-15 15:14:21 -0500

    Made the program work but kinda

--- resources/javascript/getName.js
+++ resources/javascript/getName.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
+    //number of characters to trim to
+    let chrCont = 2000;
     $('.nameButton').click(function(){
-        let nameGot = document.getElementById("trimText");
-        let nameArr = function(str){
-            
+        //text submitted
+        const NAME_GOT = document.getElementById("trimText");
+        const NAME_ARR = [...NAME_GOT.value];
+        let spamMess = [];
+        console.log(NAME_ARR.length);
+        for(let i=0; i<NAME_ARR.length;i+=chrCont){
+            spamMess.push([NAME_ARR.slice(i,chrCont+i)]);
+            console.log(spamMess[i/chrCont]);
+            spamMess[i/chrCont].join(' ');
+            console.log(spamMess[i/chrCont].join('_'));
+            let spamDispl = `<li class="spam">${spamMess[i/chrCont]}</li>`;
+            $('#spamCont').append(spamDispl);
         }
-        $('#result').text(nameGot.value);
     });
 })
